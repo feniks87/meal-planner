9df3eb41a754495a3dd2d9f6723330561cdb498d
Kate Shaitarova <eshaytarova@gmail.com>
2019-11-06 12:25:32 +1300

    to the previous commit

---
+++ src/components/Recipes/RecipeItem.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledRecipe = styled.div`
+    display: grid;
+    grid-template-columns: 10rem 1fr;
+    grid-template-rows: repeat(3, min-content);
+    row-gap: 1rem;
+    column-gap: 2rem;
+    margin-top: 5rem;
+`;
+
+const StyledImage = styled.img`
+    grid-column: 1;
+    grid-row: 1 / 4;
+    width: 10rem;
+    height: 10rem;
+`;
+
+const StyledRecipeName = styled.div`
+    grid-column: 2;
+    grid-row: 1;
+    margin-top: -.5rem;
+    font-size: 2rem;
+    font-weight: 700;
+`;
+
+const StyledRecipeIngs = styled.div`
+    grid-column: 2;
+    grid-row: 2;
+    font-size: 1.6rem;
+`;
+
+const StyledRecipeDirections = styled.div`
+    grid-column: 2;
+    grid-row: 3;
+    font-size: 1.6rem;
+`;
+
+const RecipeItem = (props) => (
+    <StyledRecipe>
+        <StyledImage />
+        <StyledRecipeName>{props.name}</StyledRecipeName>
+        <StyledRecipeIngs>{props.ings}</StyledRecipeIngs>
+        <StyledRecipeDirections>{props.directions}</StyledRecipeDirections>
+    </StyledRecipe>
+)
+
+export default RecipeItem;
